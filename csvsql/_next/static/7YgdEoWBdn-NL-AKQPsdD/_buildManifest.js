26b137bd4ca2ed117d4978168e4f892de65fcf82
Gregor Smith <gregor_smith@outlook.com>
2023-05-02 00:35:39 +0100

    add csvsql

---
+++ csvsql/_next/static/7YgdEoWBdn-NL-AKQPsdD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6342fa7117466fac.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
